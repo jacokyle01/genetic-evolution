21719336508fb486e8ead17702737e4b645bf1fe
Kyle Jacobson <jacokyle01@gmail.com>
2023-07-28 10:56:57 -0500

    Prefer composition over OOP-based

--- main.js
+++ main.js
@@ -1,78 +1,53 @@
-class Entity {
-
-    constructor(posX, posY) {
-        this.posX = posX;
-        this.posY = posY;
-        this.direction = null;
-        this.speed = 0;
-
-    }
-
-    setVelocity(speed, direction) {
-        this.speed = speed;
-        this.direction = direction;
-    }
-
-
-
-    updatePosition() {
-        switch (this.direction) {
-            case null:
-                break;
-            case "north":
-                this.posY -= this.speed;
-                break;
-            case "east":
-                this.posX += this.speed;
-                break;
-            case "south":
-                this.posY -= this.speed;
-                break;
-            case "west":
-                this.posX -= this.speed;
-                break;
-        }
-    }
-
-    draw() {
-        
-    }
-}
-
-
-
-//////////////////////////////////////////////////////
-
-
-
-
-
 const button = document.querySelector('button');
 const entities = [];
-
-
 
 let tick = 0;
 addEventListener(button, nextTick());
 
 function nextTick() {
     tick++;
-    entities.forEach(entity => entity.updatePosition());
-    entities.forEach(entity => entity.draw());
+    entities.forEach(entity => entity.update());
 }
 
-function addEntity(posX, posY) {
-    const entity = new Entity(posX, posY);
-    entities.push(entity);
+function mover (data) {
+    return {
+        move: function() {
+            switch (data.direction) {
+                case "north":
+                    data.y -= data.speed;
+                    break;
+                case "east":
+                    data.x += data.speed;
+                    break;
+                case "south":
+                    data.y -= data.speed;
+                    break;
+                case "west":
+                    data.x -= data.speed;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
 
-addEntity(10, 10);
-const myEntity = entities[0];
-console.log(myEntity);
-myEntity.setVelocity(10, "north");
-console.log(myEntity);
-myEntity.updatePosition();
-console.log(myEntity);  
+const createEntity = (kinematicData) => {
+    const entity = {
+        kinematicData
+    }
+    Object.assign(entity, mover(kinematicData));
+    return entity;
+}
+
+const createKinematicData = (x, y, speed, direction) => {
+    return {x, y, speed, direction};
+}
+
+const data = createKinematicData(0, 0, 10, "north");
+const entity = createEntity(data);
+entity.move();
+
 
 
 
